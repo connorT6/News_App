685fc3e99681348666dc324c3f2f3f3590491e6e
connorT6 <snjp.9702@gmail.com>
2022-08-25 07:57:20 +0530

    implemented bottom navigation and dashboard fragments

--- app/src/main/java/com/connort6/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/connort6/newsapp/ui/MainActivity.kt
@@ -3,16 +3,26 @@ package com.connort6.newsapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.connort6.newsapp.R
+import com.connort6.newsapp.databinding.ActivityMainBinding
 import com.connort6.newsapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        mainViewModel.testConnectivity()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.fragmentContainerMain.id) as NavHostFragment
+        binding.bottomNaviView.setupWithNavController(navHostFragment.findNavController())
     }
 }
